03972de01e59775f788ce22067cc64d8e9c883dc
Vikrant Yadav <vikrant.yadav1401@gmail.com>
2023-09-28 21:22:10 +0530

    fixing prod

--- app/components/FAQ/index.tsx
+++ app/components/FAQ/index.tsx
@@ -20,7 +20,7 @@ const FAQ = () => {
                                     />
                                 </Disclosure.Button>
                                 <Disclosure.Panel className="px-4 pt-4 pb-2 text-base text-black font-normal opacity-50">
-                                Teams are welcome to add new members as long as the total team members do not exceed the specified limit (e.g., 3 members). However, participants are prohibited from swapping or removing any existing team members.<br /> To make changes, please send your request to [Organizer's Email] with the details of the new team member requested. Include your team's unique code in the email. <br />
+                                    Teams are welcome to add new members as long as the total team members do not exceed the specified limit (e.g., 3 members). However, participants are prohibited from swapping or removing any existing team members.<br /> To make changes, please send your request to [Organizer's Email] with the details of the new team member requested. Include your team&apos;s unique code in the email. <br />
                                 </Disclosure.Panel>
                             </>
                         )}
@@ -39,7 +39,7 @@ const FAQ = () => {
                                     />
                                 </Disclosure.Button>
                                 <Disclosure.Panel className="px-4 pt-4 pb-2 text-base text-black font-normal opacity-50">
-                                Participants are requested to submit their work in PDF format. <br />
+                                    Participants are requested to submit their work in PDF format. <br />
                                 </Disclosure.Panel>
                             </>
                         )}
@@ -52,20 +52,19 @@ const FAQ = () => {
                             <>
                                 <Disclosure.Button className="flex w-full justify-between rounded-lg px-4 py-2 text-left text-2xl font-medium">
                                     <span>What is the limit or cap on the number of team members, and can anyone participate individually?
-</span>
+                                    </span>
                                     <ChevronUpIcon
                                         className={`${open ? 'rotate-180 transform' : ''
                                             } h-5 w-5 text-purple-500`}
                                     />
                                 </Disclosure.Button>
                                 <Disclosure.Panel className="px-4 pt-4 pb-2 text-base text-black font-normal opacity-50">
-                                Each team can have a maximum of four members and cannot exceed this limit. Participants who wish to participate individually can do so by filling out and submitting the details as a team leader.
+                                    Each team can have a maximum of four members and cannot exceed this limit. Participants who wish to participate individually can do so by filling out and submitting the details as a team leader.
                                 </Disclosure.Panel>
                             </>
                         )}
                     </Disclosure>
                 </div>
-
             </div>
         </div>
     )
--- app/components/Joinus/index.tsx
+++ app/components/Joinus/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 
 const Join = () => {
     return (
@@ -11,19 +12,35 @@ const Join = () => {
                 </div>
 
                 <div className="mx-auto max-w-4xl pt-5">
-                    <div className="sm:flex items-center mx-5 p-5 sm:p-0 rounded-xl justify-between bg-lightgrey sm:rounded-full">
+                    <form className="sm:flex items-center mx-5 p-5 sm:p-0 rounded-xl justify-between bg-lightgrey sm:rounded-full">
                         <div>
-                            <input type="name" className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:rounded-full bg-lightgrey pl-1 focus:outline-none bg-emailbg focus:text-black" placeholder="Your name" autoComplete="off" />
+                            <label htmlFor="name" className="sr-only">Your name</label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:rounded-full bg-lightgrey pl-1 focus:outline-none bg-emailbg focus:text-black"
+                                placeholder="Your name"
+                                autoComplete="off"
+                            />
                         </div>
                         <div>
-                            <input type="email" className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:border-l border-linegrey bg-lightgrey focus:outline-none bg-emailbg focus:text-black" placeholder="Your email" autoComplete="off" />
+                            <label htmlFor="email" className="sr-only">Your email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                className="my-4 py-4 sm:pl-6 lg:text-xl text-black sm:border-l border-linegrey bg-lightgrey focus:outline-none bg-emailbg focus:text-black"
+                                placeholder="Your email"
+                                autoComplete="off"
+                            />
                         </div>
                         <div className="sm:mr-3">
                             <button type="submit" className="joinButton w-full sm:w-0 text-xl text-white font-semibold text-center rounded-xl sm:rounded-full bg-blue hover:bg-btnblue">
                                 Join!
                             </button>
                         </div>
-                    </div>
+                    </form>
                 </div>
 
             </div>

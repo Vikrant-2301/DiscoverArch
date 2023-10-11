659a7458c06b40d90c0a03b375a410d8c1b6c900
Vikrant Yadav <vikrant.yadav1401@gmail.com>
2023-10-12 02:48:59 +0530

    deployment fix 5

--- app/Competition/page.tsx
+++ app/Competition/page.tsx
@@ -3,7 +3,7 @@ import FAQ from "../components/FAQ";
 import FirstPage from "../components/FirstPage";
 import Jury from "../components/Jury";
 import Content from "../components/Content";
-import Steps from "../components/Steps";
+// import Steps from "../components/Steps";
 import Brief from "../components/Brief";
 
 export default function page() {
